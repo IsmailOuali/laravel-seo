a217a117230e2f14d70aec925d71fba01fd6c3fe
IsmailOuali <144810840+IsmailOuali@users.noreply.github.com>
2025-08-04 16:04:36 +0100

    Seo Configuration

---
+++ config/seo.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'defaults' => [
+        'title' => 'Default Site Title',
+        'description' => 'Default site description',
+        'image' => 'https://example.com/default-image.jpg',
+    ]
+];
