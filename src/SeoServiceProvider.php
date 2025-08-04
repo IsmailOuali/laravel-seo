e0ec5a924308e286bd7c48c2806e5b3eb131520c
IsmailOuali <144810840+IsmailOuali@users.noreply.github.com>
2025-08-04 16:05:13 +0100

    Seo Service Provider

---
+++ src/SeoServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace YourVendor\LaravelSeo;
+
+use Illuminate\Support\ServiceProvider;
+
+use function app;
+
+class SeoServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->app->singleton('seo', fn () => new SeoManager());
+    }
+
+    public function boot()
+    {
+        if (!function_exists('config_path')) {
+            /**
+             * Get the configuration path.
+             *
+             * @param  string  $path
+             * @return string
+             */
+            function config_path($path = '')
+            {
+                return app()->basePath('config') . ($path ? DIRECTORY_SEPARATOR . $path : $path);
+            }
+        }
+
+        $this->publishes([
+            __DIR__.'/../config/seo.php' => config_path('seo.php'),
+        ], 'seo-config');
+
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'seo');
+    }
+}
